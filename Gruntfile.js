ff13d220b394c737b2f14f53f5ce46ed1f9256c3
Martin Heidegger <martin.heidegger@gmail.com>
2014-09-20 17:38:17 +0900

    Adding stylsprite to the build list (this way the images will be generated)

--- Gruntfile.js
+++ Gruntfile.js
@@ -61,5 +61,5 @@ module.exports = function (grunt) {
 			}
 		}
 	})
-	grunt.registerTask("default", ["jade", "stylus", "connect", "open", "watch"]);
+	grunt.registerTask("default", ["jade", "stylsprite", "stylus", "connect", "open", "watch"]);
 }
